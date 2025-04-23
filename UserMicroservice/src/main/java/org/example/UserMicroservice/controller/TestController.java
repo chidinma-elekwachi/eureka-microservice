10d5e97654bad8a5d6436faaaf7996db4ef652da
chidinma-elekwachi <elexisbiz@gmail.com>
2025-04-23 12:26:50 +0100

    Cloning git repo, connecting it to the config server

--- UserMicroservice/src/main/java/org/example/UserMicroservice/controller/TestController.java
+++ UserMicroservice/src/main/java/org/example/UserMicroservice/controller/TestController.java
@@ -5,6 +5,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class TestController {
 
@@ -20,4 +23,13 @@ public class TestController {
     public String test2(){
         return "It is working on port " + environment.getProperty("local.server.port");
     }
+    @GetMapping(value = "/get-configs")
+    public Map<String, String> configs(){
+        Map<String, String> configs = new HashMap<>();
+        configs.put("number", environment.getProperty("niit.student.number"));
+        configs.put("course", environment.getProperty("niit.student.course"));
+
+        return configs;
+    }
+
 }
