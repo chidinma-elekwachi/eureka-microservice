076384f340b20f7d9ccdfa963cf676ab8c68ab2f
chidinma-elekwachi <elexisbiz@gmail.com>
2025-05-14 11:16:06 +0100

    product microservice connecion

---
+++ UserMicroservice/src/main/java/org/example/UserMicroservice/controller/Product.java
@@ -0,0 +1,69 @@
+package org.example.UserMicroservice.controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private double price;
+    private String userId;
+    private int availableQuantity;
+    private String description;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+}
--- UserMicroservice/src/main/java/org/example/UserMicroservice/controller/UserController.java
+++ UserMicroservice/src/main/java/org/example/UserMicroservice/controller/UserController.java
@@ -1,7 +1,14 @@
 package org.example.UserMicroservice.controller;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/v1/users")
@@ -20,10 +27,24 @@ public class UserController {
         return userService.createUser(request);
     }
     @GetMapping(value = "/{userId}")
-    public User findUserById(@PathVariable("userId") String userId){
+    public ResponseEntity<?> findUserById(@PathVariable("userId") String userId) {
         User user = userService.findByUserId(userId);
 
-        //Get produc from product-service
-        return null;
-    }
+        // Get product from product-service
+        String url = "http://ProductMicroservice/api/v1/products" + userId;
+        ParameterizedTypeReference<List<Product>> responseType = new ParameterizedTypeReference<>() {};
+        ResponseEntity<List<Product>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                responseType);
+        List<Product> products = response.getBody();
+
+        Map<String, Object> returnType = new LinkedHashMap<>();
+        returnType.put("user", user);
+        returnType.put("user-products", products);
+
+        return ResponseEntity.ok(returnType);
+}
+
 }
